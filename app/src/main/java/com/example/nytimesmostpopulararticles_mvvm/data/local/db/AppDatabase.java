f21829746d93bd808f3e8a2321e40c1ade034dad
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2020-02-19 16:43:37 +0200

    change database version number

--- app/src/main/java/com/example/nytimesmostpopulararticles_mvvm/data/local/db/AppDatabase.java
+++ app/src/main/java/com/example/nytimesmostpopulararticles_mvvm/data/local/db/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 import com.example.nytimesmostpopulararticles_mvvm.data.local.db.dao.ArticleDao;
 import com.example.nytimesmostpopulararticles_mvvm.data.model.db.Article;
 
-@Database(entities = {Article.class}, version = 2)
+@Database(entities = {Article.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract ArticleDao articleDao();
